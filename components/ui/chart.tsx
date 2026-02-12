2c172e7a957c31271e1c5eaa74edd01cf0b25a07
phoenixworker16-code <phoenix.worker16@gmail.com>
2026-02-12 17:03:24 +0100

    Fix: Correction TypeScript chart.tsx avec cast any

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -218,7 +218,7 @@ const ChartTooltipContent = React.forwardRef<
                 )}
               >
                 {formatter && item.value !== undefined && item.name ? (
-                  formatter(item.value, item.name, item, index, item.payload || [])
+                  formatter(item.value, item.name, item, index, item.payload as any)
                 ) : (
                   <>
                     {itemConfig?.icon ? (
